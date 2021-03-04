7ad5c9189551db25c384d65dd9c18685a3159207
motuer97 <1617073515@qq.com>
2021-03-04 19:37:43 +0800

    change in rebet and add mert

--- mert/mert.py
+++ mert/mert.py
@@ -1,14 +1,11 @@
 import numpy as np
 from sklearn import tree
-from data.rebet.dataLoder import REBETDataLoader
+from data.mert.dataLoder import MERTDataLoader
 from model import Model
-from scipy.stats import gamma
-import math
-from scipy import stats
+
 from sklearn.metrics import mean_squared_error
 
-
-def update(trainX, trainY, epoch, n, N, m, D, q, u, σ2, clf, y0, z, M):
+def update(trainX, trainY, epoch, n, N, m, D, q, u, σ2, clf, y0, z):
     """
     
 
@@ -22,8 +19,8 @@ def update(trainX, trainY, epoch, n, N, m, D, q, u, σ2, clf, y0, z, M):
         观测次数
     m : int
         每种观测对象的观测次数
-    D:  array
-        迪利克雷参数中的协方差矩阵
+    D : array
+        随机效应变量的协方差矩阵
     q : int
         随机效应变量数
     u : array
@@ -35,151 +32,54 @@ def update(trainX, trainY, epoch, n, N, m, D, q, u, σ2, clf, y0, z, M):
     y0 : array
     z : array
         随机效应参数
-    M:  float
-        迪利克雷分布参数
 
     Returns
     -------
     None.
 
     """
-    u[0] = np.random.multivariate_normal(np.zeros(q), D, size=(1)).reshape(q, 1)
-    #print(u)
-    for i in range(n):
-        if i != 0:
-            j = i + 1
-            a = np.random.binomial(1, M / (M + j - 1), 1)
-            if a == 1:
-                u[i] = np.random.multivariate_normal(np.zeros(q), D, size=(1)).reshape(q, 1)
-
-            else:
-                b = np.random.randint(1, j)
-                u[i] = u[b - 1]
-
     for i in range(epoch):
         for j in range(n):
             y0[j * m:(j + 1) * m, 0:1] = trainY[j * m:(j + 1) * m, 0:1] - np.dot(z[j], u[j])
         clf.fit(trainX, y0)
         y1 = clf.predict(trainX).reshape(N, 1)
-
-        a = 0
-        for j in range(n):
-            #print(y1[j * m:(j + 1) * m, 0:1] , np.dot(z[j], u[j]))
-            a = a + np.dot((trainY[j * m:(j + 1) * m, 0:1] - y1[j * m:(j + 1) * m, 0:1] - np.dot(z[j], u[j])).T,
-                           (trainY[j * m:(j + 1) * m, 0:1] - y1[j * m:(j + 1) * m, 0:1] - np.dot(z[j], u[j])))
-         
-
-        a = a / 2
-        #print(a)
-        b = N / 2 + 1
-        if (a==0):
+        if (σ2==0):
             σ2 = 0.01
-        else:
-            σ2 = 1 / gamma.rvs(b, scale=1 / a)
-        if (σ2<=0.01):
-            σ2 = 0.01
-        #print(σ2)
-        r = np.empty((n, q, 1))
-        k = 0
-
-        l = np.zeros(n)
-        c = 0
-        for t in range(n):
-            f = stats.multivariate_normal.pdf((trainY[t * m:(t + 1) * m, 0:1]).reshape(m),
-                                              mean=(y1[t * m:(t + 1) * m, 0:1] + np.dot(z[t], u[t])).reshape(m),
-                                              cov=σ2 * np.identity(m))
-            c = c + stats.multivariate_normal.pdf((trainY[t * m:(t + 1) * m, 0:1]).reshape(m),
-                                                  mean=(y1[t * m:(t + 1) * m, 0:1] + np.dot(z[t], u[t])).reshape(m),
-                                                  cov=σ2 * np.identity(m))
-            l[t] = f
-
+        #print(D,σ2)
         for j in range(n):
-            a = np.identity(q)
-            d = np.identity(q)
-            Q = 1/(np.dot(z[j].T, z[j]) / σ2 + 1/D)
+            v = np.dot(np.dot(z[j], D), z[j].T) + σ2 * np.identity(m)
+            u[j] = np.dot(np.dot(np.dot(D, z[j].T), np.linalg.inv(v)),
+                          trainY[j * m:(j + 1) * m, 0:1] - y1[j * m:(j + 1) * m, 0:1])
 
-            U = np.dot(np.dot(z[j], Q), z[j].T) / σ2 - np.identity(m)
-            #print(Q,D)
-            I = M * math.pow(σ2 * 2 * math.pi, m / (-2)) * math.pow(np.abs(D), -1 / 2) * math.pow(np.abs(Q), 1 / 2) * math.exp((np.dot(
-                np.dot((trainY[j * m:(j + 1) * m, 0:1] - y1[j * m:(j + 1) * m, 0:1]).T, U),
-                trainY[j * m:(j + 1) * m, 0:1] - y1[j * m:(j + 1) * m, 0:1])) / (2 * σ2))
-
-            w = I
-            f = c - l[j]
-            p = w / (w + f)
-            if np.isnan(p) or p < 0:
-                p = 0
-            if np.isinf(p) or p > 1:
-                p = 1
-            a = np.random.binomial(1, p, 1)
-            if a == 1:
-                d = np.identity(q)
-                mean = np.dot(np.dot(
-                    np.linalg.inv(np.dot(z[j].T, z[j]) + σ2 * np.divide(d, D, out=np.zeros_like(d), where=D != 0)),
-                    z[j].T), trainY[j * m:(j + 1) * m, 0:1] - y1[j * m:(j + 1) * m, 0:1])
-                d = np.identity(q)
-                cov = σ2 * np.linalg.inv(
-                    np.dot(z[j].T, z[j]) + σ2 * np.divide(d, D, out=np.zeros_like(d), where=D != 0))
-                u[j] = np.random.multivariate_normal(mean.reshape(q), cov, size=(1)).reshape(q, 1)
-                #print(mean)
-                r[k] = u[j]
-                k = k + 1
-            else:
-                s = np.delete(l, j)
-                b = np.random.choice(np.arange(n - 1), size=1, replace=True,
-                                     p=(s) / (s.sum()))
-
-                if b < j:
-                    u[j] = u[b]
-                else:
-                    u[j] = u[b + 1]
-                a = 0
-                for t in range(k):
-                    if (r[t] == u[j]).all():
-                        a = 1
-                        break
-                if a == 0:
-                    r[k] = u[j]
-                    k = k + 1
-        r = r[0:k, :, :].reshape(k, q)
-
-        a = np.dot(r.T, r) / 2
-        b = k / 2 + 1
-        D = 1 / gamma.rvs(b, scale=np.abs(1 / a))
-        #print(D)
-        if (D<math.pow(10,-10)):
-            D = np.identity(q)
-            u[0] = np.random.multivariate_normal(np.zeros(q), D, size=(1)).reshape(q, 1)
-    #print(u)
-            for i in range(n):
-                if i != 0:
-                    j = i + 1
-                    a = np.random.binomial(1, M / (M + j - 1), 1)
-                    if a == 1:
-                        u[i] = np.random.multivariate_normal(np.zeros(q), D, size=(1)).reshape(q, 1)
-                    else:
-                        b = np.random.randint(1, j)
-                        u[i] = u[b - 1]
+        σ = 0.0
+        d = np.zeros(q)
+        for j in range(n):
+            v = np.dot(np.dot(z[j], D), z[j].T) + σ2 * np.identity(m)
+            e0 = trainY[j * m:(j + 1) * m, 0:1] - np.dot(z[j], u[j]) - y1[j * m:(j + 1) * m, 0:1]
+            σ = σ + np.dot(e0.T, e0)  # + σ2*(m - σ2*np.trace(v))
+            #print(v,m - σ2*np.trace(v),i)
+            d = d + np.dot(u[j], u[j].T) + D - np.dot(np.dot(np.dot(np.dot(D, z[j].T), np.linalg.inv(v)), z[j]), D)
+        σ2 = σ / N
+        D = d / n
+    
     return σ2
 
 
-class REBETModel(Model):
+class MERTModel(Model):
 
     def __init__(self, **kwargs):
         """
         
-        n:int,可选(默认 = 100)
+        n:int,可选(默认 = 1)
         观测对象种类数量
         q：int
         随机效应变量数
         D:array
-        迪利克雷参数中的协方差矩阵
+        随机效应变量的协方差矩阵
         u:array
         随机效应变量
         σ2:float
-        误差的方差
-        M:float,可选(默认 = 10)
-        迪利克雷分布参数
+        误差的方差 
         epoch:int,可选(默认=50)
         循环轮数
         k:int,可选(默认=1)
@@ -190,10 +90,6 @@ class REBETModel(Model):
             self.n = 1
         else:
             self.n = kwargs["n"]
-        if "M" not in kwargs.keys():
-            self.M = 10
-        else:
-            self.M = kwargs["M"]
         if "epoch" not in kwargs.keys():
             self.epoch = 50
         else:
@@ -207,11 +103,11 @@ class REBETModel(Model):
             self.ga = 0
         else:
             self.ga = kwargs["ga"]
-        self.q = 1
+        self.q = 2
         self.D = np.identity(self.q)
-        self.u = np.zeros((self.n, 1,1))
-        self.σ2 = 0.1
-        
+        self.u = np.zeros((self.n, self.q, 1))
+        self.σ2 = 1.0
+
     def fitForUI(self, **kwargs):
         """ 返回结果到前端
         :return:
@@ -239,7 +135,6 @@ class REBETModel(Model):
             returnDic = {
               "提示":"使用参数寻优，显示寻优参数值",
               "epoch": str(x[0][0].astype(int)),
-              "M": str(x[2][0]),
               "k": str(x[1][0].astype(int)),
               "mean_squared_error": str(mse)
             }
@@ -261,6 +156,7 @@ class REBETModel(Model):
 
         """
         
+       
         N:int
         观测次数
         m:int
@@ -269,38 +165,38 @@ class REBETModel(Model):
         随机效应参数
 
         """
-
         self.trainX = kwargs["trainX"]
         self.trainY = kwargs["trainY"]
-        self.trainY = self.trainY * 10
-            
-        self.clf = tree.DecisionTreeRegressor(criterion='mse',ccp_alpha=0.1, max_depth=(self.trainX.shape[1]))
-        N = self.trainX.shape[0]
+        self.clf = tree.DecisionTreeRegressor(criterion='mse', ccp_alpha=0.1, max_depth=(self.trainX.shape[1]))
+        N = self.trainY.shape[0]
         self.trainY = self.trainY.reshape(N,1)
         m = int(N / self.n)
-        z = np.zeros((self.n, m, self.q))
+        z = np.ones((self.n, m, self.q))
         if(self.k!=0):
             for i in range(self.n):
-                z[i:i + 1, :, 0:1] = z[i:i + 1, :, 0:1]  + self.trainX[m * i:m * (i + 1), self.k-1:self.k]
-        #print(z)
+                z[i:i + 1, :, 1:2] = z[i:i + 1, :, 1:2] - 1 + self.trainX[m * i:m * (i + 1), self.k-1:self.k]
+        if(self.k==0):
+            z = np.zeros((self.n, m, self.q))
         y0 = np.empty((N, 1))
-        self.σ2 = update(self.trainX, self.trainY, self.epoch, self.n, N, m, self.D, self.q, self.u, self.σ2, self.clf, y0, z, self.M)
+        self.σ2 = update(self.trainX, self.trainY, self.epoch, self.n, N, m, self.D, self.q, self.u, self.σ2, self.clf, y0, z)
         for j in range(self.n):
             y0[j * m:(j + 1) * m, 0:1] = self.trainY[j * m:(j + 1) * m, 0:1] - np.dot(z[j], self.u[j])
 
         self.clf2 = tree.DecisionTreeRegressor(criterion='mse', random_state=0,ccp_alpha=0.1, max_depth=(self.trainX.shape[1]))
         self.clf2.fit(self.trainX, y0)
+        # tree.plot_tree(self.clf2,filled=True)
+        x0 = self.clf2.predict(self.trainX).reshape(N, 1)
+        for j in range(self.n):
+            x0[j * m:(j + 1) * m, 0:1] = x0[j * m:(j + 1) * m, 0:1] + np.dot(z[j], self.u[j])
         
-        #print(z,self.u,np.dot(z[j], self.u[j]), self.clf2.predict(self.trainX),self.clf2.predict(self.trainX).reshape(N, 1)+np.dot(z[j], self.u[j]))
-        
-           
+       
 
     def test(self, **kwargs):
         self.testX = kwargs["testX"]
         self.testY = kwargs["testY"]
         if (self.ga==1):
-            N = 3
-            m = [[50,200],[1,self.trainX.shape[1]],[0.01,100]]
+            N = 2
+            m = [[50,200],[1,self.trainX.shape[1]]]
             precisions = 24
             N_GENERATIONS = 50
             POP_SIZE = 50
@@ -309,44 +205,56 @@ class REBETModel(Model):
             model1 = GAModel(n=self.n,testX=self.testX, testY=self.testY, trainX=self.trainX, trainY=self.trainY, N=N, m=m, precisions=precisions, N_GENERATIONS=N_GENERATIONS, 
                              POP_SIZE=POP_SIZE,MUTATION_RATE=MUTATION_RATE, CROSSOVER_RATE=CROSSOVER_RATE)
             y,x = model1.predict()
-            self.model = REBETModel(n=self.n, epoch=x[0][0].astype(int), M=x[2][0], k=x[1][0].astype(int))
+            self.model = MERTModel(n=self.n, epoch=x[0][0].astype(int), k=x[1][0].astype(int))
             self.model.fit(trainX=self.trainX, trainY=self.trainY)
             N = self.testX.shape[0]
             m = int(N / self.n)
-            z = np.zeros((self.n, m, self.q))
+            z = np.ones((self.n, m, self.q))
             if(self.k!=0):
                 for i in range(self.n):
-                    z[i:i + 1, :, 0:1] = z[i:i + 1, :, 0:1]  + self.testX[m * i:m * (i + 1), self.k-1:self.k] 
+                    z[i:i + 1, :, 1:2] = z[i:i + 1, :, 1:2] - 1 + self.testX[m * i:m * (i + 1), self.k-1:self.k]
+            
+            if(self.k==0):
+                z = np.zeros((self.n, m, self.q))
+                self.σ2 = 0
             x0 = self.clf2.predict(self.testX).reshape(N, 1)
             for j in range(self.n):
                 x0[j * m:(j + 1) * m, 0:1] = x0[j * m:(j + 1) * m, 0:1] + np.dot(z[j], self.u[j]) + self.σ2
-            mse = mean_squared_error(x0/10, self.testY)
+            mse = mean_squared_error(x0, self.testY)
             return x,mse
         N = self.testX.shape[0]
         m = int(N / self.n)
-        z = np.zeros((self.n, m, self.q))
+        z = np.ones((self.n, m, self.q))
         if(self.k!=0):
             for i in range(self.n):
-                z[i:i + 1, :, 0:1] = z[i:i + 1, :, 0:1]  + self.testX[m * i:m * (i + 1), self.k-1:self.k]
+                z[i:i + 1, :, 1:2] = z[i:i + 1, :, 1:2] - 1 + self.testX[m * i:m * (i + 1), self.k-1:self.k]
+                
+        if(self.k==0):
+            z = np.zeros((self.n, m, self.q))
+            self.σ2 = 0
         x0 = self.clf2.predict(self.testX).reshape(N, 1)
-        #print(x0,self.testX)
         for j in range(self.n):
             x0[j * m:(j + 1) * m, 0:1] = x0[j * m:(j + 1) * m, 0:1] + np.dot(z[j], self.u[j]) + self.σ2
-        #print(x0)
-        return mean_squared_error(x0/10, self.testY)
+        
+        return mean_squared_error(x0, self.testY)
     
     def predict(self, **kwargs):
         self.predictX = kwargs["predictX"]
         N = self.predictX.shape[0]
         m = int(N / self.n)
-        z = np.zeros((self.n, m, self.q))
+        z = np.ones((self.n, m, self.q))
         if(self.k!=0):
             for i in range(self.n):
-                z[i:i + 1, :, 0:1] = z[i:i + 1, :, 0:1] + self.predictX[m * i:m * (i + 1), self.k-1:self.k]
+                z[i:i + 1, :, 1:2] = z[i:i + 1, :, 1:2] - 1 + self.predictX[m * i:m * (i + 1), self.k-1:self.k]
+                
+        if(self.k==0):
+            z = np.zeros((self.n, m, self.q))
+            self.σ2 = 0
         x0 = self.clf2.predict(self.predictX).reshape(N, 1)
         for j in range(self.n):
             x0[j * m:(j + 1) * m, 0:1] = x0[j * m:(j + 1) * m, 0:1] + np.dot(z[j], self.u[j]) + self.σ2
-        return x0/10
+        return x0
+
 
 
 
@@ -364,7 +272,7 @@ def get_fitness(n,trainX,trainY,testX,testY,pop, N, precisions, m, POP_SIZE):
     y2 = x[1].astype(int)
     pred = np.zeros([POP_SIZE,])
     for i in range(POP_SIZE):    
-        model = REBETModel(n=n, epoch=y1[i], M=x[2][i], k=y2[i], ga=0)
+        model = MERTModel(n=n, epoch=y1[i], k=y2[i], ga=0)
         model.fit(trainX=trainX, trainY=trainY)
         pred[i] = model.test(testX=testX, testY=testY)
     return -(pred - np.max(pred)) + 1e-10
@@ -459,7 +367,7 @@ def get_fitness1(n,trainX,trainY,testX,testY,pop, N, precisions, m, POP_SIZE):
     y1 = x[0].astype(int)
     y2 = x[1].astype(int)
     for i in range(POP_SIZE):    
-        model = REBETModel(n=n, epoch=y1[i], M=x[2][i], k=y2[i], ga=0)
+        model = MERTModel(n=n, epoch=y1[i], k=y2[i], ga=0)
         model.fit(trainX=trainX, trainY=trainY)
         pred[i] = model.test(testX=testX, testY=testY)
     return pred
@@ -563,21 +471,19 @@ class GAModel(Model):
     def predict(self):
         return ga(self.n,self.trainX,self.trainY,self.testX,self.testY, self.N, self.m, self.precisions, self.N_GENERATIONS, self.POP_SIZE, self.MUTATION_RATE, self.CROSSOVER_RATE)
 
+
 if __name__ == '__main__':
     n = 1
-    epoch = 200
-    k = 2
-    M = 1
-    datapath1="../data/rebet/data_train.xlsx"
-    datapath2="../data/rebet/data_test.xlsx"
-    datapath3="../data/rebet/data_predict.xlsx"
-    dataloader = REBETDataLoader()
+    epoch = 50
+    k = 3
+    datapath1="../data/mert/data_train.xlsx"
+    datapath2="../data/mert/data_test.xlsx"
+    datapath3="../data/mert/data_predict.xlsx"
+    dataloader = MERTDataLoader()
     trainX, trainY = dataloader.loadTrainData(train_path=datapath1)
-    testX, testY = dataloader.loadTestData(test_path=datapath2)
+    textX, textY = dataloader.loadTestData(test_path=datapath2)
     predictX = dataloader.loadPredictData(predict_path=datapath3)
-    model = REBETModel(n=n, epoch=epoch, M=M, k=k ,ga=0)
-    model.fitForUI(trainX=trainX, trainY=trainY)
-    print(model.testForUI(testX=testX, testY=testY))
-    print(model.predictForUI(predictX=predictX))
-    
-    
+    model = MERTModel(n=n, epoch=epoch, k=k ,ga=0)
+    print(model.fitForUI(trainX=trainX, trainY=trainY))
+    print(model.testForUI(testX=textX, testY=textY))
+    print(model.predictForUI(predictX=textX))
